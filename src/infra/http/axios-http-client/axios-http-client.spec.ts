16d4d788c10ea555df36a340b9de368582275e34
fredowisk <zcrazyhero@gmail.com>
2023-05-10 16:01:13 -0300

    refactor: move SUT creation to a factory method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -6,12 +6,16 @@ import faker from 'faker'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
+
 describe('Axios Http Client', () => {
   test('should call axios with correct URL', async () => {
     const url = faker.internet.url()
 
-    const sut = new AxiosHttpClient()
-    await sut.post({ url })
+    await makeSut().post({ url })
+
     expect(mockedAxios).toHaveBeenCalledWith(url)
   })
 })
