5b0b39959d9f6d81ebbd12b4ce4e4b13e8134399
fredowisk <zcrazyhero@gmail.com>
2023-05-10 09:20:09 -0300

    chore: add more configs to husky hooks

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,23 +1,23 @@
-import { RemoteAuthentication } from "./remote-authentication";
-import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 type SutTypes = {
-  sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
-};
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
 
-const makeSut = (url: string = "any_url"): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
-  const sut = new RemoteAuthentication(url, httpPostClientSpy);
-  return { sut, httpPostClientSpy };
-};
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return { sut, httpPostClientSpy }
+}
 
-describe("Remote Authentication", () => {
-  test("should call HttpPostClient with correct URL ", async () => {
-    const url = "other_url";
-    const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.auth();
+describe('Remote Authentication', () => {
+  test('should call HttpPostClient with correct URL ', async () => {
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth()
 
-    expect(httpPostClientSpy.url).toBe(url);
-  });
-});
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
