6afeedf5e2cb0f139f3f9d701afb6b5439db1859
fredowisk <zcrazyhero@gmail.com>
2023-05-16 11:07:17 -0300

    fix: change webpack env var to use JSON.stringify

--- webpack.config.js
+++ webpack.config.js
@@ -17,23 +17,30 @@ module.exports = {
     }
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: './public',
@@ -47,7 +54,9 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      'process.env.API_URL': 'http://fordevs.herokuapp.com/api/'
+      'process.env.API_URL': JSON.stringify(
+        'http://fordevs.herokuapp.com/api/'
+      )
     })
   ]
 }
